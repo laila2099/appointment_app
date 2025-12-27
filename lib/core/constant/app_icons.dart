f8bff2e1e00ab426613b887e29e91993aacbb68f
laila2099 <laila.sankar@gmail.com>
2025-12-27 23:44:08 +0300

    Add app_icons.dart and update pubspec.yaml

---
+++ lib/core/constant/app_icons.dart
@@ -0,0 +1,8 @@
+class AppIcons {
+  static const String basePath = "assets/icons/";
+
+  static const String home = '${basePath}home-2.svg';
+  static const String search = '${basePath}search.svg';
+  static const String chat = '${basePath}chat.svg';
+  static const String calendar = '${basePath}calendar.svg';
+}
