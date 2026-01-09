417453ac66cafde30613758693ca018a5bd104a9
SarahAlghadir <sarahalghadir7@gmail.com>
2026-01-09 19:25:15 +0100

    Update app_config.dart

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
   AppConfig._();
 
-  static const baseUrl = 'https://hlpmplopjolzmntqrjky.supabase.co';
-  static const apikey ='eyJhbG ciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhscG1wbG9wam9sem1udHFyamt5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczMzIzMDAsImV4cCI6MjA4MjkwODMwMH0.1hUeoTyBoeLE0GmXi5QudhDxxGBIWvQ3Mdowhcf_qn4';
+  static const baseUrl = '';
+  static const apikey ='';
 }
