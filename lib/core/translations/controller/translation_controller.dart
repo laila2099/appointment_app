e4f0edc8b111ef42016ebe3d7bc1e7cfdfd60a95
laila2099 <laila.sankar@gmail.com>
2026-01-11 17:01:39 +0300

    fixing color and adding translations

--- lib/core/translations/controller/translation_controller.dart
+++ lib/core/translations/controller/translation_controller.dart
@@ -1,40 +1,3 @@
-// import 'dart:ui';
-// import 'package:appointment_app/core/services/shared_prefrences.dart';
-// import 'package:get/get.dart';
-
-// class TranslationController extends GetxController {
-//   late final AppPreferencesService myServices;
-
-//   var locale = const Locale('en', 'US').obs;
-
-//   final Map<String, Locale> supportedLocales = {
-//     'en': const Locale('en', 'US'),
-//     'ar': const Locale('ar', 'SY'),
-//     'fr': const Locale('fr', 'FR'),
-//   };
-
-//   void changeLocale(Locale newLocale) {
-//     locale.value = newLocale;
-//     myServices.setString("lang", newLocale.languageCode);
-//     Get.updateLocale(newLocale);
-//   }
-
-//   @override
-//   void onInit() {
-//     super.onInit();
-//     myServices = Get.find<AppPreferencesService>();
-
-//     String langCode = myServices.getString("lang") ?? "en";
-
-//     if (supportedLocales.containsKey(langCode)) {
-//       locale.value = supportedLocales[langCode]!;
-//     } else {
-//       locale.value = const Locale('en', 'US');
-//     }
-
-//     Get.updateLocale(locale.value);
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:appointment_app/core/services/shared_prefrences.dart'; // تأكد من المسار
@@ -42,13 +5,10 @@ import 'package:appointment_app/core/services/shared_prefrences.dart'; // تأك
 class TranslationController extends GetxController {
   late final AppPreferencesService myServices;
 
-  // المفتاح الموحد للحفظ
   static const String langKey = 'lang_code';
 
-  // القيمة المبدئية
   var locale = const Locale('en', 'US').obs;
 
-  // قائمة اللغات المدعومة
   final Map<String, Locale> supportedLocales = {
     'en': const Locale('en', 'US'),
     'ar': const Locale('ar', 'SY'),
@@ -67,13 +27,11 @@ class TranslationController extends GetxController {
       final newLocale = supportedLocales[languageCode]!;
       locale.value = newLocale;
       Get.updateLocale(newLocale);
-      // حفظ الكود فقط (ar, en, fr)
       myServices.setString(langKey, languageCode);
     }
   }
 
   void _loadLanguage() {
-    // قراءة الكود المحفوظ، إذا لم يوجد نأخذ الإنجليزية كافتراضي
     String? sharedPrefLang = myServices.getString(langKey);
 
     if (sharedPrefLang != null &&
